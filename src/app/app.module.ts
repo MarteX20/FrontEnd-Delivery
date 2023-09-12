607f9239eecf3fb26c9d4328507a96b86d0b3948
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-09-12 14:13:53 +0200

    aggiunta Tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './auth/login/login/login.component';
-import { RegisterComponent } from './auth/register/register/register.component';
 
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'login', component: LoginComponent },
+]
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    LoginComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
