93fb3f61eeef3d75dac49f0181b6777d960b9f9d
MarteX20 <98113914+MarteX20@users.noreply.github.com>
2023-09-12 15:13:43 +0200

    configurazione tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{html,ts}",
+        "./node_modules/flowbite/**/*.js" // add this line
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
-
